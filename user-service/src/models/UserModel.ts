12b7dd669e3f5ceb5603db7fc54c7948d0751e29
anjulasjay99 <anjulasjay@gmail.com>
2025-10-04 21:24:52 +0530

    user and mentor profile creation

---
+++ user-service/src/models/UserModel.ts
@@ -0,0 +1,53 @@
+import { Schema, model, Document } from "mongoose";
+import { Role } from "../types/Enums";
+
+export interface IUser extends Document {
+    email: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    role: Role;
+    active: boolean;
+    createdAt: string;
+    updatedAt: string;
+}
+
+const UserSchema = new Schema<IUser>({
+    email: {
+        type: String,
+        required: true,
+        index: true,
+    },
+    password: {
+        type: String,
+        trim: true,
+    },
+    firstName: {
+        type: String,
+        trim: true,
+    },
+    lastName: {
+        type: String,
+        trim: true,
+    },
+    role: {
+        type: String,
+        enum: Object.values(Role),
+        required: true,
+        default: Role.MENTEE,
+    },
+    active: {
+        type: Boolean,
+        default: true,
+    },
+    createdAt: {
+        type: String,
+        trim: true,
+    },
+    updatedAt: {
+        type: String,
+        trim: true,
+    },
+});
+
+export const User = model<IUser>("User", UserSchema);
---
+++ user-service/src/routes/userRoutes.ts
@@ -0,0 +1,74 @@
+import { Router } from "express";
+import { createUser } from "../services/userService";
+import bcrypt from "bcryptjs";
+import { Role } from "../types/Enums";
+import { createMentorProfile } from "../services/mentorService";
+import { Types } from "mongoose";
+
+const router = Router();
+
+router.get("/", (_req, res) => {
+    res.json({ message: "User endpoint working" });
+});
+
+router.post("/", async (_req, res) => {
+    const { email, password, firstName, lastName, role } = _req.body;
+
+    const createdAt = new Date().toISOString();
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    try {
+        const user = await createUser({
+            email,
+            password: hashedPassword,
+            firstName,
+            lastName,
+            role,
+            createdAt,
+            updatedAt: createdAt,
+            active: true,
+        });
+
+        if (role === Role.MENTEE) {
+            res.status(200).json({
+                message: "Successfully created the user",
+                user,
+            });
+        } else {
+            const {
+                designation,
+                bio,
+                domains,
+                yearsOfExperience,
+                hourlyRate,
+                badges,
+                socials,
+            } = _req.body.mentorProfile;
+
+            const mentorProfile = await createMentorProfile({
+                userId: user._id as Types.ObjectId,
+                designation,
+                bio,
+                domains,
+                yearsOfExperience,
+                hourlyRate,
+                badges,
+                socials,
+                createdAt,
+                updatedAt: createdAt,
+            });
+
+            res.status(200).json({
+                message: "Successfully created the user",
+                user,
+                mentorProfile,
+            });
+        }
+    } catch (error: any) {
+        res.status(error.statusCode).json({
+            message: error.message,
+        });
+    }
+});
+
+export default router;
