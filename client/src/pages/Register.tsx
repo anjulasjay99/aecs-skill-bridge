a467c928148aac9bc681859caead4d563e6301fb
Anjula Jayasinghe <83394637+anjulasjay99@users.noreply.github.com>
2025-10-26 19:14:40 +0530

    destroy resources

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -364,7 +364,7 @@ const Register = () => {
 
             <footer className="bg-white py-4 border-t border-gray-100">
                 <div className="container mx-auto px-4 text-center text-gray-500 text-sm">
-                    <p>©2025 EventPal. All rights reserved</p>
+                    <p>©2025 Skill Bridge. All rights reserved</p>
                 </div>
             </footer>
         </div>
